69c6822ad75c0d9a5c011def68fe03d92509bd50
lucasmoraist <luksmnt1101@gmail.com>
2025-02-13 11:22:12 -0300

    feat: restrict CORS allowed origins to localhost for development

--- src/main/java/com/sobei/banco_de_talentos/infra/security/SecurityConfig.java
+++ src/main/java/com/sobei/banco_de_talentos/infra/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5174/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
