47fd8c583ccdb9952b145692902f2089ae359616
lucasmoraist <luksmnt1101@gmail.com>
2025-03-01 13:35:37 -0300

    feat: add findByFilters method to CandidatoRepository for enhanced querying with cargo, status, and regiao filters

--- src/main/java/com/sobei/banco_de_talentos/repository/CandidatoRepository.java
+++ src/main/java/com/sobei/banco_de_talentos/repository/CandidatoRepository.java
@@ -1,7 +1,30 @@
 package com.sobei.banco_de_talentos.repository;
 
+import com.sobei.banco_de_talentos.domain.enums.CargoEnum;
+import com.sobei.banco_de_talentos.domain.enums.StatusEnum;
 import com.sobei.banco_de_talentos.domain.model.Candidato;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface CandidatoRepository extends MongoRepository<Candidato, String> {
+    @Query("""
+        {
+            $and: [
+                { "cargo": ?0 },
+                { $or: [
+                    { "status": ?1 },
+                    { "?1": null }
+                ] },
+                { $or: [
+                    { "endereco.regiao": { $regex: ?2, $options: 'i' } },
+                    { "?2": ".*" },
+                    { "endereco.regiao": { $exists: false } }
+                ] }
+            ]
+        }
+        """)
+    Page<Candidato> findByFilters(CargoEnum cargo, StatusEnum status, String regiao, Pageable pageable);
+
 }
